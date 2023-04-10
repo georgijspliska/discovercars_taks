97679af4ea50584458327dc1f1e6312475d35f99
georgijspliska <georgijspliska@gmail.com>
2023-04-10 23:52:49 +0300

    added post feature

--- server.js
+++ server.js
@@ -61,6 +61,101 @@ app.get('/api/environment', (req, res) => {
     }
 });
 
+app.get('/api/headers', (req, res) => {
+  const headers = Object.entries(req.headers);
+  const arr = [];
+  const bgColor = '#ZZZZZZ';  // <- Backgroud Color Here 
+  const fgColor = '#ZZZZZZ';  // <- Foregroud Color Here 
+
+  for (const [key, value] of headers) {
+    arr.push({ key, value });
+  }
+
+  const format = req.query.format || 'html';
+
+  switch (format) {
+    case 'html':
+        const html_res = `
+          <html>
+            <head>
+              <style>
+                body {
+                  background-color: ${CheckColor(bgColor,'#FFFFFF')};
+                  color: ${CheckColor(fgColor,'#000000')};
+                }
+              </style>
+            </head>
+            <body>
+              ${arr.map(({key, value}) => `<p>${key}: ${value}</p>`).join('')}
+            </body>
+          </html>
+        `;
+        res.type('html').send(html_res);
+        break;
+    case 'json':
+        res.type('json').send(JSON.stringify(arr));
+        break;
+    case 'xml':
+        const xml = `<head>${arr.map(({key, value}) => `<${key}>${value}</${key}>`).join('')}</head>`;
+        res.type('xml').send(xml);
+        break;
+    default:
+        res.send(`Invalid format`);
+}
+
+});
+
+
+app.use('/api/post', (req, res) => {
+
+  const method = req.method;
+
+  if (method !== 'POST') {
+    res.status(405).send('Method not allowed');
+    return;
+  }
+
+  const body = Object.entries(req.body);
+  const arr = [];
+  const bgColor = '#ZZZZZZ';  // <- Backgroud Color Here 
+  const fgColor = '#ZZZZZZ';  // <- Foregroud Color Here 
+
+  for (const [key, value] of body) {
+    arr.push({ key, value });
+  }
+
+  switch (format) {
+    case 'html':
+        const html_res = `
+          <html>
+            <head>
+              <style>
+                body {
+                  background-color: ${CheckColor(bgColor,'#FFFFFF')};
+                  color: ${CheckColor(fgColor,'#000000')};
+                }
+              </style>
+            </head>
+            <body>
+              ${arr.map(({key, value}) => `<p>${key}: ${value}</p>`).join('')}
+            </body>
+          </html>
+        `;
+        res.type('html').send(html_res);
+        break;
+    case 'json':
+        res.type('json').send(JSON.stringify(arr));
+        break;
+    case 'xml':
+        const xml = `<body>${arr.map(({key, value}) => `<${key}>${value}</${key}>`).join('')}</body>`;
+        res.type('xml').send(xml);
+        break;
+    default:
+        res.send(`Invalid format`);
+  }
+
+});
+
 app.listen(PORT, HOST, () => {
     console.log(`Running on http://${HOST}:${PORT}`);
 });
