d780c1bc1c5f0e033b34c45df5b741c5763667dd
georgijspliska <georgijspliska@gmail.com>
2023-04-10 23:08:55 +0300

    added html colors

--- server.js
+++ server.js
@@ -6,6 +6,14 @@ const HOST = '0.0.0.0';
 
 const app = express();
 
+function CheckColor(color,def) {
+  const regex = /^#([0-9A-F]{3}){1,2}$/i;
+  if (regex.test(color)) {
+    return color;
+  }
+  return def;
+}
+
 app.get('/', (req, res) => {
     res.send('Hello World Discover Cars');
 });
@@ -13,6 +21,8 @@ app.get('/', (req, res) => {
 app.get('/api/environment', (req, res) => {
     const env = Object.entries(process.env);
     const arr = [];
+    const bgColor = '#ZZZZZZ';  // <- Backgroud Color Here 
+    const fgColor = '#ZZZZZZ';  // <- Foregroud Color Here 
   
     for (const [key, value] of env) {
       arr.push({ key, value });
@@ -22,7 +32,21 @@ app.get('/api/environment', (req, res) => {
     
     switch (format) {
         case 'html':
-            const html_res = arr.map(({key, value}) => `<p>${key}: ${value}</p>`).join('');
+            const html_res = `
+              <html>
+                <head>
+                  <style>
+                    body {
+                      background-color: ${CheckColor(bgColor,'#FFFFFF')};
+                      color: ${CheckColor(fgColor,'#000000')};
+                    }
+                  </style>
+                </head>
+                <body>
+                  ${arr.map(({key, value}) => `<p>${key}: ${value}</p>`).join('')}
+                </body>
+              </html>
+            `;
             res.type('html').send(html_res);
             break;
         case 'json':
